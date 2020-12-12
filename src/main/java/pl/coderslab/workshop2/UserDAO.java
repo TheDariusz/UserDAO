7c6cf91feff613865d4a6164f541ca328be6bde5
TheDariusz <thedariusz@gmail.com>
2020-12-12 17:21:01 +0100

    clear main

--- src/main/java/pl/coderslab/workshop2/UserDAO.java
+++ src/main/java/pl/coderslab/workshop2/UserDAO.java
@@ -5,14 +5,5 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class UserDAO {
-    public static void main(String[] args) {
-        String query = "SELECT * FROM users";
 
-        try (Connection conn = DBUtil.getConnection()){
-            DBUtil.printAllData(conn, query);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-    }
 }
