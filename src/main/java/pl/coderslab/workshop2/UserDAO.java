91742be28612cbbf8b9a76c5b5c373494c1931f0
TheDariusz <thedariusz@gmail.com>
2020-12-12 22:36:46 +0100

    UserDAO class

--- src/main/java/pl/coderslab/workshop2/UserDAO.java
+++ src/main/java/pl/coderslab/workshop2/UserDAO.java
@@ -1,9 +1,41 @@
 package pl.coderslab.workshop2;
 
+
+import pl.coderslab.bcrypt.BCrypt;
+
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class UserDAO {
+    private static final String CREATE_USER_QUERY =
+            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+    private static final String SELECT_USER_QUERY =
+            "SELECT * FROM users WHERE id=?";
 
+    public User create(User user) {
+        try (Connection conn = DBUtil.getConnection()) {
+            PreparedStatement stmt =
+                    conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS);
+            stmt.setString(1, user.getUserName());
+            stmt.setString(2, user.getEmail());
+            stmt.setString(3, hashPassword(user.getPassword()));
+            stmt.executeUpdate();
+
+            ResultSet rs = stmt.getGeneratedKeys();
+            if (rs.next()) {
+                user.setId(rs.getInt(1));
+            }
+            return user;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public String hashPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
 }
