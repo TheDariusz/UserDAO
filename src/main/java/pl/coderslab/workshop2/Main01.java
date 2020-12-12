4a98ba15e73222b71719d5214545b464952b2752
TheDariusz <thedariusz@gmail.com>
2020-12-12 22:44:24 +0100

    create user

--- src/main/java/pl/coderslab/workshop2/Main01.java
+++ src/main/java/pl/coderslab/workshop2/Main01.java
@@ -2,6 +2,11 @@ package pl.coderslab.workshop2;
 
 public class Main01 {
     public static void main(String[] args) {
+        User user1 = new User();
+        user1.setUserName("mauricio");
+        user1.setEmail("mau@gmail.com");
+        user1.setPassword("lola");
+
 
     }
 }
