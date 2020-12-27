652f379c8cc507f5981537c3a1c63be0e2c87eb5
TheDariusz <thedariusz@gmail.com>
2020-12-27 23:45:28 +0100

    code reformatted

--- src/main/java/pl/coderslab/workshop2/User.java
+++ src/main/java/pl/coderslab/workshop2/User.java
@@ -1,47 +1,47 @@
 package pl.coderslab.workshop2;
 
 public class User {
-    private int id;
-    private String userName;
-    private String email;
+  private int id = 0;
+  private String userName;
+  private String email;
 
-    public User(String userName, String email, String password) {
-        this.userName = userName;
-        this.email = email;
-        this.password = password;
-    }
+  public User(String userName, String email, String password) {
+    this.userName = userName;
+    this.email = email;
+    this.password = password;
+  }
 
-    private String password;
+  private String password;
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void setId(int id) {
+    this.id = id;
+  }
 
-    public String getUserName() {
-        return userName;
-    }
+  public String getUserName() {
+    return userName;
+  }
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+  public void setUserName(String userName) {
+    this.userName = userName;
+  }
 
-    public String getEmail() {
-        return email;
-    }
+  public String getEmail() {
+    return email;
+  }
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+  public void setEmail(String email) {
+    this.email = email;
+  }
 
-    public String getPassword() {
-        return password;
-    }
+  public String getPassword() {
+    return password;
+  }
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
--- src/main/java/pl/coderslab/workshop2/UserDAO.java
+++ src/main/java/pl/coderslab/workshop2/UserDAO.java
@@ -1,94 +1,87 @@
 package pl.coderslab.workshop2;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import pl.coderslab.bcrypt.BCrypt;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import pl.coderslab.bcrypt.BCrypt;
 
 public class UserDAO {
-    private static Logger logger = LogManager.getLogger(UserDAO.class);
+  private static Logger logger = LogManager.getLogger(UserDAO.class);
 
-    private static final String CREATE_USER_QUERY =
-            "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
-    private static final String UPDATE_USER_QUERY =
-            "UPDATE users SET username=?, email=?, password=? WHERE id=?";
-    private static final String CHECK_EMAIL_QUERY =
-            "SELECT users.id FROM users WHERE email=? and id<>?";
+  private static final String CREATE_USER_QUERY =
+      "INSERT INTO users(username, email, password) VALUES (?, ?, ?)";
+  private static final String UPDATE_USER_QUERY =
+      "UPDATE users SET username=?, email=?, password=? WHERE id=?";
+  private static final String CHECK_EMAIL_QUERY =
+      "SELECT users.id FROM users WHERE email=? and id<>?";
 
-    public User create(User user) {
-        if (user==null){
-            return null;
-        }
-        if (emailAlreadyExists(user.getId(), user.getEmail())) {
-            logger.error("User with email {} already exists!", user.getEmail());
-            return null;
-        }
-
-        try (Connection conn = DBUtil.getConnection();
-             PreparedStatement stmt =
-                     conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS)) {
-            stmt.setString(1, user.getUserName());
-            stmt.setString(2, user.getEmail());
-            stmt.setString(3, hashPassword(user.getPassword()));
-            stmt.executeUpdate();
-
-            ResultSet rs = stmt.getGeneratedKeys();
-            if (rs.next()) {
-                user.setId(rs.getInt(1));
-            }
-            return user;
-        } catch (SQLException e) {
-            logger.error("Create user query failed!", e);
-            e.printStackTrace();
-            return null;
-        }
+  public User create(User user) {
+    if (user == null) {
+      return null;
+    }
+    if (emailAlreadyExists(user.getId(), user.getEmail())) {
+      logger.error("User with email {} already exists!", user.getEmail());
+      return null;
     }
 
-    public void update(User user) {
-        if (emailAlreadyExists(user.getId(), user.getEmail())) {
-            logger.error("User with email {} already exists!", user.getEmail());
-            return;
-        }
-        try (Connection conn = DBUtil.getConnection();
-             PreparedStatement stmt = conn.prepareStatement(UPDATE_USER_QUERY)) {
-            stmt.setString(1, user.getUserName());
-            stmt.setString(2, user.getEmail());
-            stmt.setString(3, hashPassword(user.getPassword()));
-            stmt.setInt(4, user.getId());
-            stmt.executeUpdate();
+    try (Connection conn = DBUtil.getConnection();
+        PreparedStatement stmt =
+            conn.prepareStatement(CREATE_USER_QUERY, Statement.RETURN_GENERATED_KEYS)) {
+      stmt.setString(1, user.getUserName());
+      stmt.setString(2, user.getEmail());
+      stmt.setString(3, hashPassword(user.getPassword()));
+      stmt.executeUpdate();
 
-        } catch (SQLException e) {
-            logger.error("Update query failed!", e);
-            e.printStackTrace();
-        }
+      ResultSet rs = stmt.getGeneratedKeys();
+      if (rs.next()) {
+        user.setId(rs.getInt(1));
+      }
+      logger.info("User {id: {}, email: {}} created in DB!", user.getId(), user.getEmail());
+      return user;
+    } catch (SQLException e) {
+      logger.error("Create user query failed!", e);
+      return null;
     }
+  }
 
-    private boolean emailAlreadyExists(int id, String email) {
-        try (Connection conn = DBUtil.getConnection();
-             PreparedStatement stmnt = conn.prepareStatement(CHECK_EMAIL_QUERY)) {
-            stmnt.setString(1, email);
-            stmnt.setInt(2, id);
-            ResultSet rs = stmnt.executeQuery();
-            if (rs.next()){
-                return true;
-            }
-        } catch (SQLException e) {
-            logger.error("Check email query failed!", e);
-            e.printStackTrace();
-        }
-        return false;
+  public void update(User user) {
+    if (emailAlreadyExists(user.getId(), user.getEmail())) {
+      logger.warn("User with email {} already exists!", user.getEmail());
+      return;
     }
+    try (Connection conn = DBUtil.getConnection();
+        PreparedStatement stmt = conn.prepareStatement(UPDATE_USER_QUERY)) {
+      stmt.setString(1, user.getUserName());
+      stmt.setString(2, user.getEmail());
+      stmt.setString(3, hashPassword(user.getPassword()));
+      stmt.setInt(4, user.getId());
+      stmt.executeUpdate();
 
-    private String hashPassword(String password) {
-        return BCrypt.hashpw(password, BCrypt.gensalt());
+    } catch (SQLException e) {
+      logger.error("Update query failed!", e);
     }
+  }
 
+  private boolean emailAlreadyExists(int id, String email) {
+    try (Connection conn = DBUtil.getConnection();
+        PreparedStatement stmnt = conn.prepareStatement(CHECK_EMAIL_QUERY)) {
+      stmnt.setString(1, email);
+      stmnt.setInt(2, id);
+      ResultSet rs = stmnt.executeQuery();
+      if (rs.next()) {
+        return true;
+      }
+    } catch (SQLException e) {
+      logger.error("Check email query failed!", e);
+    }
+    return false;
+  }
+
+  private String hashPassword(String password) {
+    return BCrypt.hashpw(password, BCrypt.gensalt());
+  }
 }
-
-
-
