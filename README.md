d7b256ccb0d26202500d654b6f0a2b05e93ede4e
TheDariusz <thedariusz@gmail.com>
2021-01-06 14:08:36 +0100

    commit after refreshed

--- README.md
+++
@@ -1,2 +0,0 @@
-# UserDAO
-Object-oriented access layer to the MySQL database
--- src/main/java/pl/coderslab/workshop2/exceptions/EmailDuplicateException.java
+++
@@ -1,7 +0,0 @@
-package pl.coderslab.workshop2.exceptions;
-
-public class EmailDuplicateException extends RuntimeException {
-  public EmailDuplicateException(String message) {
-    super(message);
-  }
-}
